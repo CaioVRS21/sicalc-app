4594debd3542b6f522d4288aed718841cd7d3fb5
Caio Veras <caioveras@MacBook-Air-de-Caio.local>
2025-08-21 22:48:39 -0400

    projeto finalizado + faltava coisa

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.meuapp.sicalc',
+  appName: 'sicalc-app',
+  webDir: 'dist/sicalc-app/browser'
+};
+
+export default config;
